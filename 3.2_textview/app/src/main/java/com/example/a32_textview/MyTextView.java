87406e978321a733fa549a5537b268e994366985
zcq <1322688717@qq.com>
2022-03-16 23:12:13 +0800

    textview 常见属性及其自定义TextView

---
+++ 3.2_textview/app/src/main/java/com/example/a32_textview/MyTextView.java
@@ -0,0 +1,27 @@
+package com.example.a32_textview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+public class MyTextView extends androidx.appcompat.widget.AppCompatTextView {
+
+    public MyTextView(Context context) {
+        super(context);
+    }
+
+    public MyTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean isFocused() {
+        return true;
+    }
+}
