8a843fc3533545c44f09d2de27569f51796f6312
zhaichaoqun <81673615+1322688717@users.noreply.github.com>
2023-02-27 16:38:03 +0800

    新建项目并且格式化代码

--- jectpack/jetpack_ViewModel_LiveData_DataBinding/KotlinViewModelDemo/app/src/main/java/com/example/kotlinviewmodeldemo/MainActivity.kt
+++ jectpack/jetpack_ViewModel_LiveData_DataBinding/KotlinViewModelDemo/app/src/main/java/com/example/kotlinviewmodeldemo/MainActivity.kt
@@ -12,14 +12,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
 
-        //ViewModel绑定
+        // ViewModel绑定
         val selfViewModel: SelfViewModel = ViewModelProvider(this).get(SelfViewModel::class.java)
 
-        //dataBinding 将View上的控件绑定到binding
+        // dataBinding 将View上的控件绑定到binding
         val binding: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        binding.data = selfViewModel //给xml文件中的data赋值
-        binding.lifecycleOwner = this //生命周期
+        binding.data = selfViewModel // 给xml文件中的data赋值
+        binding.lifecycleOwner = this // 生命周期
     }
 }
--- jectpack/jetpack_ViewModel_LiveData_DataBinding/KotlinViewModelDemo/app/src/main/java/com/example/kotlinviewmodeldemo/SelfViewModel.kt
+++ jectpack/jetpack_ViewModel_LiveData_DataBinding/KotlinViewModelDemo/app/src/main/java/com/example/kotlinviewmodeldemo/SelfViewModel.kt
@@ -4,9 +4,9 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class SelfViewModel : ViewModel() {
-    //声明一个私有变量number
+    // 声明一个私有变量number
     private val _number: MutableLiveData<Int> by lazy {
-        //MutableLiveData是LiveData的一个容器，Int是数据类型
+        // MutableLiveData是LiveData的一个容器，Int是数据类型
         MutableLiveData<Int>().also {
             it.value = 0
         }
